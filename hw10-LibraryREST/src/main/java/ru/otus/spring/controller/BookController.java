1b5b44ec32c785c3078b71097318282ed684ee3e
Ekaterina <katrina_n@mail.ru>
2021-02-17 01:16:30 +0300

    Домашнее задание #10. Приложение-библиотека с использованием REST + jQuery

--- hw10-LibraryREST/src/main/java/ru/otus/spring/controller/BookController.java
+++ hw10-LibraryREST/src/main/java/ru/otus/spring/controller/BookController.java
@@ -3,22 +3,12 @@ package ru.otus.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import ru.otus.spring.service.DbServiceAuthor;
-import ru.otus.spring.service.DbServiceBook;
-import ru.otus.spring.service.DbServiceGenre;
 
 
 @Controller
 public class BookController {
 
-    final private DbServiceBook dbServiceBook;
-    final private DbServiceGenre dbServiceGenre;
-    final private DbServiceAuthor dbServiceAuthor;
-
-    public BookController(DbServiceBook dbServiceBook, DbServiceGenre dbServiceGenre, DbServiceAuthor dbServiceAuthor) {
-        this.dbServiceBook = dbServiceBook;
-        this.dbServiceGenre = dbServiceGenre;
-        this.dbServiceAuthor = dbServiceAuthor;
+    public BookController() {
     }
 
     @GetMapping("/books")
