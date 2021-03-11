ab1307638b1df2c67014a7da5c229efde253d2ff
Ekaterina <katrina_n@mail.ru>
2021-03-12 01:24:52 +0300

    Домашнее задание #12. Приложение-библиотека с аутентификацией

--- hw12–Authentication/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
+++ hw12–Authentication/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
 
     @Override
     public void configure(WebSecurity web) {
